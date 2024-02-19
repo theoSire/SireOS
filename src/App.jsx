635ce06a8dee68d02e8bff5ea6b14372fa6d820c
Theofrid <theofridthiyadore@gmail.com>
2024-02-19 13:52:37 -0500

    BaseApp - created a base app layout including a titlebar and a workspace - added resizability and dragability to the app
    Terminal
    - started implementing a terminal app using the baseapp
    - added functionality to focus on the input element when the workspace is
      selected

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
-import {useRef} from "react"
-import { useDraggable } from "@neodrag/react"
-import "./style.css"
+import Terminal from "./apps/Terminal"
 
 export default function App() {
-  const draggableRef = useRef(null);
-  useDraggable(draggableRef, { handle: '.titlebar' }, { bounds: 'parent' })
-
   return (
     <>
-      <div className="window" ref={draggableRef}>
-        <div className="titlebar">
-          <button className="close-button"></button>
-          <span>Terminal</span>
-        </div>
-        <div className="workspace">
-          <span>workspace</span>
-        </div>
-      </div>
+      <Terminal></Terminal>
     </>
   )
 }
-
---
+++ src/apps/BaseApp.jsx
@@ -0,0 +1,23 @@
+import {useRef, useState} from "react"
+import { useDraggable } from "@neodrag/react"
+import "../style.css"
+
+export default function BaseApp(props) {
+  const [title, setTitle] = useState(props.title || '')
+  const [content, setContent] = useState(props.content || '')
+
+  const draggableRef = useRef(null);
+  useDraggable(draggableRef, { handle: '.titlebar', bounds: 'body' })
+
+  return (
+    <div className="window" ref={draggableRef}>
+      <div className="titlebar">
+       <button className="close-button"></button>
+       <span>{title}</span>
+      </div>
+      <div className="content">
+        {content}
+     </div>
+    </div>
+  )
+}
