0fdb3119b9a1db30c2e28e1e85aae525f1e6397f
Theofrid <theofridthiyadore@gmail.com>
2024-03-24 17:49:55 -0400

    - Finished porting the states to the store - Made changes so that maximized windows cannot be resized

--- src/apps/BaseApp.jsx
+++ src/apps/BaseApp.jsx
@@ -1,16 +1,13 @@
-import {useEffect, useRef, useState} from "react"
+import { useEffect, useRef, useState } from "react"
 import { useDraggable } from "@neodrag/react"
 import { useAppStore, useBaseAppStore } from "../store"
 import '../style.css'
 
 export default function BaseApp({ appKey, content }) {
-  const [maximized, setMaximized] = useState(true)
-  const [isVisible, setIsVisible] = useState(true)
-
   const draggableRef = useRef(null);
   useDraggable(draggableRef, { handle: '.titlebar', bounds: 'body' })
 
-  const { apps } = useBaseAppStore()
+  const { apps, toggleMaximize, toggleVisibility } = useBaseAppStore()
   const p = apps[appKey]
   const { closeApp } = useAppStore()
 
@@ -19,26 +16,27 @@ export default function BaseApp({ appKey, content }) {
       ref={draggableRef}
       style={{
               boxShadow: '0px 0px 1rem black',
-              transform: maximized ? 'translate(0px, 0px)' : 'translate(0%, 0%)',
-              top: maximized ? '0' : '0%',
-              left: maximized ? '0' : '0%',
-              height: maximized ? '100vh' : p.height, 
-              width: maximized ? '100vw' : p.width,
+              resize: p.maximized ? 'none' : 'both',
+              transform: p.maximized ? 'translate(0px, 0px)' : 'translate(0%, 0%)',
+              top: p.maximized ? '0' : '0%',
+              left: p.maximized ? '0' : '0%',
+              height: p.maximized ? '100vh' : p.height, 
+              width: p.maximized ? '100vw' : p.width,
               minHeight: p.minHeight,
               minWidth: p.minWidth,
-              opacity: isVisible ? 1 : 0,
+              opacity: p.isVisible ? 1 : 0,
             }}
     >
       <div className="titlebar relative flex items-center cursor-default" 
         onDoubleClick={() => { 
-            setMaximized(!maximized)
+            toggleMaximize(appKey)
         }}
       >
         <button 
           className={`close-button ml-1.5 border-2 border-slate-400 p-1.5 text-xs rounded-lg hover:bg-slate-300 hover:border-slate-300 active:bg-slate-500 active:border-slate-500 transition-colors duration-300`} 
           onClick={() => { if (closeApp) {
             setTimeout(() => {
-              setIsVisible(!isVisible)
+              toggleVisibility(appKey)
               closeApp(appKey) 
             }, 150)
             }}}
@@ -46,7 +44,7 @@ export default function BaseApp({ appKey, content }) {
         </button>
         <button 
           className={`min-max-button ml-1.5 border-2 border-orange-300 border-opacity-70 p-1.5 text-xs rounded-lg hover:bg-orange-200 hover:border-orange-200 active:bg-orange-400 active:border-orange-400 transition-colors duration-300`} 
-          onClick={() => { setMaximized(!maximized) }}>            
+          onClick={() => { toggleMaximize(appKey) }}>            
         </button>
        <span className="titlebar-text relative ml-1.5">{appKey}</span>
       </div>
